91642bf7171df4f7d10472b47fffd0fc63ca6ed8
Frederik <frederik_kok@icloud.com>
2018-01-14 17:03:11 +0100

    Refactored all for loops to apply family

--- main.R
+++ main.R
@@ -7,7 +7,6 @@
 # For further information see github: https://github.com/fkoh111/fetcher     #
 ##############################################################################
 
-#x <- "fkoh111"
 
 # fetcher() takes one arguments: either a Twitter username or a user id
 fetcher <- function (x) {
@@ -31,11 +30,10 @@ chunk_follower_ids <- split(follower_ids, (seq(nrow(follower_ids))-1) %/% 90000)
 
 
 # Writing chunk_follower_ids to temp_path
-#for (i in 1:length(chunk_follower_ids)) {
-#  write.table(chunk_follower_ids[i], row.names = FALSE, col.names = FALSE, file=paste0(names(chunk_fol#lower_ids)[i], ".txt"))
-#}
-
-lapply(chunk_follower_ids, function(x) write.table(x, row.names = FALSE, col.names = FALSE, file = paste0(names(x), ".txt")))
+mapply(
+  write.table,
+  x = chunk_follower_ids, row.names = FALSE, col.names = FALSE, file=paste(names(chunk_follower_ids), "txt", sep=".")
+)
 
 
 # Listing and reading chunk_follower_ids from temp_path
@@ -44,15 +42,12 @@ ids <- lapply(filenames, read.table)
 
 
 # From chunk_follower_ids user data is fetched. Rate limit is avoided by sleeping after each chunk_follower_ids have been looked up
-followers <- rep(NA, length(list.files()))
-if(length(filenames) > 1)
-  for (i in seq_along(ids)) {
-    followers[i] <- lapply(ids[i], lookup_users)
-    message("Avoiding rate limit by sleeping for 15 minutes at ", paste(format(Sys.time(), format = '%H:%M:%S')))
-    Sys.sleep(15*60)
+if(length(filenames) > 1) {
+  followers <- lapply(ids, lookup_users)
+  message("Avoiding rate limit by sleeping for 15 minutes at ", paste(format(Sys.time(), format = '%H:%M:%S')))
+  Sys.sleep(15*60)
   } else {
-    for (i in seq_along(ids)) {
-      followers[i] <- lapply(ids[i], lookup_users)}}
+  followers <- lapply(ids, lookup_users)}
 
 
 # Binding followers user data via data.table
@@ -68,5 +63,4 @@ return(binded_followers)
 
 
 # Function usage
-fetched_followers <- fetcher("hadleywickham")
-fetched_followers
+fetched_followers <- fetcher("mkrasnik")
