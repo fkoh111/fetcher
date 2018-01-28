356c305ed9de4b3768ec99b93a946d78fbace2df
Frederik <frederik_kok@icloud.com>
2018-01-28 17:58:04 +0100

    v. 0.9.5

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 ##############################################################################
-# FETCHER (v. 0.9.4)                                                         #
+# FETCHER (v. 0.9.5)                                                         #
 # A UDF for the rtweet package making it easy for R users to                 #
 # fetch followers' user data from Twitter accounts without having to worry   #
 # about rate limits and users with more than 90.000 followers.               #
@@ -8,12 +8,14 @@
 ##############################################################################
 
 
-# fetcher() takes two arguments: x = a Twitter username or a user id. y = a path to a chosen output folder for temporary files
-fetcher <- function (x, y = NULL) {
+# fetcher() takes two arguments: x = a Twitter username or a user id. y = a path to a chosen output folder for temporary files (y argument is optional)
+fetcher <- function(x, y = NULL) {
 
-# Setting folder for follower ids. On exit the directory will be reset
+require(rtweet)
+  
+# Setting tmp_path and folder for follower ids. On exit the directory will be reset
 root <- getwd()
-if(!is.null(y)) {
+if (!is.null(y)) {
   tmp_path <- tempfile(pattern = x, tmpdir = y)
 } else {
   tmp_path <- tempfile(pattern = x, tmpdir = tempdir())
@@ -35,21 +37,18 @@ message("Starting to fetch ", paste(n_follower_ids), " follower IDs. Expects to 
 # Fetching follower ids; if rate limit is encountered: sleep for 15 minutes
 follower_ids <- get_followers(x, n = as.integer(n_follower_ids), parse = TRUE, retryonratelimit = TRUE, verbose = FALSE)
 
-# Spliting follower_ids into n chunk_follower_ids, each chunk containing a maximum of 90.000 Twitter ids
-chunk_follower_ids <- split(follower_ids, (seq(nrow(follower_ids))-1) %/% param_users)
+# Spliting follower_ids into n chunk_follower_ids with max size of of 90.000 Twitter ids
+chunk_follower_ids <- split(follower_ids, (seq(nrow(follower_ids)) - 1) %/% param_users)
 
-# Writing chunk_follower_ids to temp_path
-mapply(
-  write.table,
-  x = chunk_follower_ids, row.names = FALSE, col.names = FALSE, file = paste(names(chunk_follower_ids), "txt", sep = ".")
-)
+# Writing chunk_follower_ids to tmp_path
+mapply(write.table, x = chunk_follower_ids, row.names = FALSE, col.names = FALSE, file = paste(names(chunk_follower_ids), "txt", sep = "."))
 
-# Listing and reading chunk_follower_ids from temp_path
+# Listing and reading chunk_follower_ids from tmp_path
 filenames <- list.files(path = tmp_path, pattern = "*.txt", full.names = TRUE)
 ids <- lapply(filenames, read.table)
 
 # Estimating and printing when the lookup_users process will be done
-if(length(filenames) > 1) {
+if (length(filenames) > 1) {
   users_estimate <- format(Sys.time() + length(filenames) * param_sleep, format = '%H:%M:%S')
   message("Starting to look up users. Expects to be done at ", paste(users_estimate), ".")  
 } else {
@@ -58,19 +57,20 @@ if(length(filenames) > 1) {
 
 # From chunk_follower_ids user data is fetched. Rate limit is avoided by sleeping after each chunk_follower_ids have been looked up
 followers <- rep(NA, length(filenames))
-if(length(filenames) > 1)
+if (length(filenames) > 1)
   for (i in seq_along(ids)) {
   followers[i] <- lapply(ids[i], lookup_users)
-  if (i > length(filenames) -1){
+  if (i > length(filenames) - 1){
     break
   }
   sleep_estimate <- format(Sys.time() + param_sleep, format = '%H:%M:%S')
   message("Avoiding rate limit by sleeping for 15 minutes. Will start again at approximately ", paste(sleep_estimate), ".")
-  Sys.sleep(param_sleep)
+  Sys.sleep (param_sleep)
   } else {
     for (i in seq_along(ids)) {
-    followers[i] <- lapply(ids[i], lookup_users)}
+    followers[i] <- lapply(ids[i], lookup_users)
     }
+  }
 
 # Binding followers user data
 binded_followers <- do_call_rbind(followers)
@@ -83,4 +83,4 @@ return(binded_followers)
 }
 
 # Function usage
-fetched_followers <- fetcher("mkrasnik", "~/Desktop/")
+fetched_followers <- fetcher("fkoh111", "~/Desktop/")
--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
-## Fetcher v. 0.9.4
+## Fetcher v. 0.9.5
 
 **Author:** Frederik Kok Hansen  
 frederik_kok@icloud.com
 
 Fetcher is a UDF for the rtweet package.  
-The purpose of fetcher is **#1** to make it easy for R users to fetch followers' user data from Twitter accounts having more than 90.000 followers and **#2** dealing with rate limits in an automated manner.  
+The purpose of fetcher is **#1** to make it easy for R users to fetch followers' user data from Twitter accounts having more than 90.000 followers and **#2** dealing with rate limits in an automated manner.
 
-Overall the function `fetcher()` is a wrapper for the `lookup_users()` function. Hence the two functions takes the same type of arguments. That is either a *user ID* or a *screen name* of a Twitter account whose followers you want to fetch.
+
+Overall the function `fetcher()` is a wrapper for the `lookup_users()` function. Hence the two functions takes the same type of arguments. That is either a *user ID* or a *screen name* of a Twitter account whose followers you want to fetch. For debuggning purposes an optional path can be set for temporary user id lists. This is particularly useful if you want to cross-check user ids with fetched user data. If the path is not supplied to fetcher on call the base R `tempdir()` function will be used instead.
 
 As with the functions from the rtweet package, it is assumed that you have a non-exhausted Twitter token in your environment for the function to work properly. 
 
-Please note that the function has only been tested on **nix* OS.
+Please note that the function has only been tested on **nix* OS but shoould work on Windows as well.
 
 For any questions please don't hesitate to contact me on GitHub or by e-mail. Also, any pull requests and proposals are more than welcome.
