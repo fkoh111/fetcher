580fa1f201bf698f92c7e0831c1456528987a841
Frederik <frederik_kok@icloud.com>
2018-03-29 01:07:43 +0200

    updated .gitignored

--- debugger.R
+++
@@ -1,8 +0,0 @@
-library("rtweet")
-
-account <- "tommyannfeldt"
-followers <- get_followers(account)
-mapply(write.table, x = followers, row.names = FALSE, col.names = FALSE, file = paste(names(followers), "csv", sep = "."))
-listed_ids <- list.files(pattern = "user_id.csv", full.names = TRUE)
-read_ids <- lapply(listed_ids, read_twitter_csv)
-test <- lapply(read_ids, lookup_users)
